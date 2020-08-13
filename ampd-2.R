5b6fcfeb6ba68a5931f53794f856b0853ed759d8
Munshi Md Rasel <munshimdrasel@gwu.edu>
2020-08-12 20:59:40 -0400

    reading file one by one (not recommended) See ampd-2.1.R file

--- ampd-2.R
+++ ampd-2.R
@@ -23,6 +23,7 @@ setwd ("/Users/munshirasel/Google Drive/R/ampd-2/data")
 #1995 File download
 
 
+
 tmp_filename <- tempfile()
 url <- "ftp://newftp.epa.gov/DMDnLoad/emissions/daily/quarterly/1995/DLY_1995alQ1.zip"
 download.file(url, tmp_filename)
@@ -51,4 +52,62 @@ unzip(tmp_filename)
 e4 <- fread("DLY_1995alQ4.csv")
 file.remove(tmp_filename)
 
+
+##########
+tmp_filename <- tempfile()
+url <- "ftp://newftp.epa.gov/DMDnLoad/emissions/daily/quarterly/1995/DLY_1995flQ1.zip"
+download.file(url, tmp_filename)
+unzip(tmp_filename)
+e5 <- fread("DLY_1995flQ1.csv")
+file.remove(tmp_filename)
+
+tmp_filename <- tempfile()
+url <- "ftp://newftp.epa.gov/DMDnLoad/emissions/daily/quarterly/1995/DLY_1995flQ2.zip"
+download.file(url, tmp_filename)
+unzip(tmp_filename)
+e6 <- fread("DLY_1995flQ2.csv")
+file.remove(tmp_filename)
+
+tmp_filename <- tempfile()
+url <- "ftp://newftp.epa.gov/DMDnLoad/emissions/daily/quarterly/1995/DLY_1995flQ3.zip"
+download.file(url, tmp_filename)
+unzip(tmp_filename)
+e7 <- fread("DLY_1995flQ3.csv")
+file.remove(tmp_filename)
+
+tmp_filename <- tempfile()
+url <- "ftp://newftp.epa.gov/DMDnLoad/emissions/daily/quarterly/1995/DLY_1995flQ4.zip"
+download.file(url, tmp_filename)
+unzip(tmp_filename)
+e8 <- fread("DLY_1995flQ4.csv")
+file.remove(tmp_filename)
+
+##########
+tmp_filename <- tempfile()
+url <- "ftp://newftp.epa.gov/DMDnLoad/emissions/daily/quarterly/1995/DLY_1995ilQ1.zip"
+download.file(url, tmp_filename)
+unzip(tmp_filename)
+e9 <- fread("DLY_1995ilQ1.csv")
+file.remove(tmp_filename)
+
+tmp_filename <- tempfile()
+url <- "ftp://newftp.epa.gov/DMDnLoad/emissions/daily/quarterly/1995/DLY_1995ilQ2.zip"
+download.file(url, tmp_filename)
+unzip(tmp_filename)
+e10 <- fread("DLY_1995ilQ2.csv")
+file.remove(tmp_filename)
+
+tmp_filename <- tempfile()
+url <- "ftp://newftp.epa.gov/DMDnLoad/emissions/daily/quarterly/1995/DLY_1995ilQ3.zip"
+download.file(url, tmp_filename)
+unzip(tmp_filename)
+e11 <- fread("DLY_1995ilQ3.csv")
+file.remove(tmp_filename)
+
+tmp_filename <- tempfile()
+url <- "ftp://newftp.epa.gov/DMDnLoad/emissions/daily/quarterly/1995/DLY_1995ilQ4.zip"
+download.file(url, tmp_filename)
+unzip(tmp_filename)
+e12 <- fread("DLY_1995ilQ4.csv")
+file.remove(tmp_filename)
 
